e484a034066780f16abc8426be1e441e82edc5ec
Tom Erik Støwer <testower@gmail.com>
2014-07-31 14:15:22 +0200

    Live search

--- wp-content/themes/boksok.no-theme/templates/searchform.php
+++ wp-content/themes/boksok.no-theme/templates/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
   <div class="input-group">
-    <input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="search-field form-control" placeholder="Søk etter en forfatter, en tittel eller et tema!  ">
+    <input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="search-field form-control" placeholder="Søk etter en forfatter, en tittel eller et tema!  " data-swplive="true">
     <label class="hide">Søk etter</label>
     <span class="input-group-btn">
       <button type="submit" class="search-submit btn btn-default">Søk</button>
